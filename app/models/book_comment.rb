49a6e07cf45986a80c64bf0139851d6a16d05db9
EC2 Default User <ec2-user@ip-172-31-47-241.ap-northeast-1.compute.internal>
2022-08-01 08:57:12 +0000

    [finish]task5

--- app/models/book_comment.rb
+++ app/models/book_comment.rb
@@ -1,4 +1,19 @@
 class BookComment < ApplicationRecord
   belongs_to :user
   belongs_to :book
+# 検索方法分岐
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @book = Book.where("title LIKE?","#{word}")
+    elsif search == "forward_match"
+      @book = Book.where("title LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @book = Book.where("title LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @book = Book.where("title LIKE?","%#{word}%")
+    else
+      @book = Book.all
+    end
   end
+
+end
